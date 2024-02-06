c5d75fd94e90bfe56f7aeee0ac78f15d5cde6087
jenyatopol <jeka010@walla.com>
2024-02-06 17:23:03 +0200

    variables.tf for vpn-tf

---
+++ terraform/aws/openvpn/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "public_cidrs" {
+    type = list
+}
+
+variable "private_cidrs" {
+    type = list
+}
+
+variable "public_sn_count" {
+    type = number
+}
+
+variable "private_sn_count" {
+    type = number
+}
