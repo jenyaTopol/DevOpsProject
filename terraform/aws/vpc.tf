47e752218b1ab1154d1b7c5fd094e76a854c57d3
jenyatopol <jeka010@walla.com>
2024-07-04 12:41:33 +0300

    wip terraform-eks

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-    source   = ./vpc
+    source   = "./vpc"
     vpc_cidr            =  "10.10.0.0/16"
     public_sn_count     = 1
     private_sn_count    = 1
-    public_cidr         = [for i in range 2,255,2 : cidersubnet("10.10.0.0/16", 8, i)]
-    private_cidr        = [for i in range 1,255,2 : cidersubnet("10.10.0.0/16", 8, i)]
+    public_cidr         = [for i in range(2, 255, 2): cidrsubnet("10.10.0.0/16", 8, i)]
+    private_cidr        = [for i in range (1,255,2): cidersubnet("10.10.0.0/16", 8, i)]
     aws_db_subnet_group = true
 
 }
