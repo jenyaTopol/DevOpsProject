d686ad030ad1e4c3293e76acc6e4af1d0ef3e0d7
jenyatopol <jeka010@walla.com>
2025-01-03 18:12:34 +0200

    wip terraform infrustructure

--- terraform/aws/vpc.tf
+++ terraform/aws/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.0.0.0/24"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
